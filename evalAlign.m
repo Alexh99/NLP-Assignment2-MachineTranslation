1464495e99acf51a0fe52affabc78d599e167c5c
Jake Stolee <jakestolee@gmail.com>
2016-03-11 01:18:21 -0500

    Updated `evalAlign.m`

--- evalAlign.m
+++ evalAlign.m
@@ -7,44 +7,68 @@
 % some of your definitions
 trainDir     = 'Hansard/Training/';
 testDir      = 'Hansard/Testing/';
-fn_LME       = 'part5_english_model.mat';
-fn_LMF       = 'part5_french_model.mat';
+fn_LME       = 'english_model.mat';
+fn_AMFE      = 'models/10 iterations/ibm1_1000.mat';
+fn_scores    = 'scores_1000.mat';
+num_sents    = 1000;
+max_iter     = 10;
 lm_type      = '';
-delta        = 1;
-vocabSize    = 1; 
-numSentences = 1000;
+delta        = 1; % These isn't used
+vocabSize    = 1; % This isn't used
 
 % Train your language models. This is task 2 which makes use of task 1
-LME = LM;% lm_train( trainDir, 'e', fn_LME );
-%LMF = lm_train( trainDir, 'f', fn_LMF );
+if exist(fn_LME, 'file') == 2
+    load(fn_LME, '-mat');
+    LME = LM;
+else
+    LME = lm_train( trainDir, 'e', fn_LME );
+end
 
-% Train your alignment model of French, given English 
-AMFE = AM; %align_ibm1( trainDir, numSentences,10,'align_model_1000.mat');
-% ... TODO: more 
+% Train your alignment model of French, given English
+if exist(fn_AMFE, 'file') == 2
+    load(fn_AMFE, '-mat');
+    AMFE = AM;
+else
+    AMFE = align_ibm1( trainDir, num_sents, max_iter, fn_AMFE );
+end
 
-% TODO: a bit more work to grab the English and French sentences. 
-%       You can probably reuse your previous code for this 
-
+% Read in lines
 lines_f = textread([testDir, filesep, 'Task5.f'], '%s','delimiter','\n');
 lines_e = textread([testDir, filesep, 'Task5.e'], '%s','delimiter','\n');
 lines_e_google = textread([testDir, filesep, 'Task5.google.e'], '%s','delimiter','\n');
-eng = {};
 
+% Keep track of the translated senteces and their BLEU scores.
+eng = {};
 scores = {};
 for l=1:length(lines_f)
-    [status, result] = unix( sprintf('curl --insecure -u "e61cf647-0223-4b73-bfb4-42e375a14af6":"s0SWfR64mL1s" -X POST -F "text=%s" -F "source=fr" -F "target=en" "https://gateway.watsonplatform.net/language-translation/api/v2/translate"', lines_f{l}) );
+    % Make a call to Bluemix to translate the French sentence
+    [status, result] = unix( sprintf(strjoin({
+        'curl --insecure', ...
+        '-u "e61cf647-0223-4b73-bfb4-42e375a14af6":"s0SWfR64mL1s"', ...
+        '-X POST', ...
+        '-F "text=%s"', ...
+        '-F "source=fr"', ...
+        '-F "target=en"', ...
+        '"https://gateway.watsonplatform.net/language-translation/api/v2/translate"', ...
+    }), lines_f{l}) );
+    % Process the French sentence
 	fre = preprocess(lines_f{l}, 'f');
-	eng{l} = decode( fre, LME, AMFE, '', delta, vocabSize );
+    % Decode into most likely Enlish sentence
+	eng{l} = decode( fre, LME, AMFE, lm_type, delta, vocabSize );
 	
+    % Process al reference sentences and split them
     refs = {
-        strsplit(' ', preprocess(lines_e{l}, 'e'), 'omit'),
-        strsplit(' ', preprocess(lines_e_google{l}, 'e'), 'omit'),
+        strsplit(' ', preprocess(lines_e{l}, 'e'), 'omit'), ...
+        strsplit(' ', preprocess(lines_e_google{l}, 'e'), 'omit'), ...
         strsplit(' ', preprocess(result, 'e'), 'omit')
     };
     
+    % Calculate the BLEU score for n=[1..3]
     scores{l} = zeros(1,3);
     for i = 1:3
         score = bleu(eng{l}, refs, i, Inf);
         scores{l}(i) = score;
     end
 end
+
+save(fn_scores, 'scores');
